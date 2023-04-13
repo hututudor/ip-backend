2fcaa89881abf845d83859b072581c496def9c16
Chichirau Razvan <chichiraurazvan@yahoo.com>
2023-04-13 07:25:03 +0300

    Made models for each table in db

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import { Model } from './Model';
+import { Model } from '.';
 
 export interface User extends Model {
   username: string;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,5 @@
 export * from './User';
 export * from './Model';
+export * from './Lobby';
+export * from './Will';
+export * from './Message';
