3a09d8934f5166b0e9d0cee3ef9a68f178b54131
Chichirau Razvan <chichiraurazvan@yahoo.com>
2023-04-29 09:33:35 +0300

    Fixed migrations

--- src/repositories/migrations/20230413042849_createTables.ts
+++ src/repositories/migrations/20230413042849_createTables.ts
@@ -13,23 +13,23 @@ export async function up(knex: Knex): Promise<void> {
     .createTable('lobbies', table => {
       table.increments('id').primary();
       table.string('gameEngineID', 256).notNullable();
-      table.specificType('userIds', 'text[]');
+      table.specificType('userIDs', 'text[]');
       table.date('createdAt').defaultTo(knex.fn.now());
       table.date('updatedAt').defaultTo(knex.fn.now());
     })
     .createTable('wills', table => {
       table.increments('id').primary();
       table.string('data').nullable();
-      table.string('userId', 256).notNullable();
+      table.string('userID', 256).notNullable();
       table.date('createdAt').defaultTo(knex.fn.now());
       table.date('updatedAt').defaultTo(knex.fn.now());
     })
     .createTable('messages', table => {
       table.increments('id').primary();
       table.string('data').notNullable();
-      table.string('userId', 256).notNullable();
+      table.string('userID', 256).notNullable();
       table.string('type', 256).notNullable();
-      table.string('lobbyId', 256).notNullable();
+      table.string('lobbyID', 256).notNullable();
       table.date('createdAt').defaultTo(knex.fn.now());
       table.date('updatedAt').defaultTo(knex.fn.now());
     });
