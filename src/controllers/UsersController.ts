28ed2a49f9ed178ddc8d261b75c8f8225a2eafbe
Stefan Vulpe <svulpe25@gmail.com>
2023-04-29 18:00:11 +0300

    login sample

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -2,16 +2,49 @@ import {
   Response as ExpressResponse,
   Request as ExpressRequest,
 } from 'express';
+import joi from 'joi';
+import jwt from 'jsonwebtoken';
+const { verify, sign } = jwt;
+
 import { Request, Response } from '../utils';
 import { User } from '../models';
+import { UsersRepository } from '../repositories';
 
 const users: User[] = [];
 
-export const login = (req: Request) => {
-  const credentials: string = req.body;
-  // TODO()
+export const login = async (req: Request) => {
+  const credentials: { username: string; password: string } = req.body;
+  const { error } = joi
+    .object({
+      username: joi.string().min(3).max(30).required(),
+      password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{5,30}$')),
+    })
+    .validate(credentials);
+
+  if (error) {
+    return Response.badRequest({ message: error.message });
+  }
+
+  let accessToken;
+
+  try {
+    const result = await new UsersRepository().findByUserName(
+      credentials.username,
+    );
+
+    if (!result.user) {
+      throw new Error(`User ${credentials.username} has not been found`);
+    }
+
+    accessToken = sign(result.user, process.env.ACCESS_SECRET_KEY!);
+  } catch (err) {
+    if (err instanceof Error) {
+      return Response.unauthorized({ message: err.message });
+    }
+  }
+
   return Response.success({
-    token: '12345',
+    token: accessToken,
   });
 };
 
--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -1,8 +1,22 @@
 import { Repository } from './Repository';
 import { User } from '../models/User';
+import { knex } from '../db';
 
 export class UsersRepository extends Repository<User> {
   getTableName(): string {
     return 'users';
+  }
+  async findByUserName(username: string) {
+    return knex(this.getTableName())
+      .select('*')
+      .where({
+        username: username,
+      })
+      .then(users => {
+        return { user: users[0] };
+      })
+      .catch(err => {
+        throw new Error(err.message);
+      });
   }
 }
