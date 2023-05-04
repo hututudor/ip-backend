a779cd9ddaa3b02a8e5c459bb4b1fe715348e0f9
Dgeorgealex <95629219+Dgeorgealex@users.noreply.github.com>
2023-05-04 10:01:12 +0300

    added messagecontroller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * as UsersController from './UsersController';
 export * as LobbiesController from './LobbiesController';
 export * as WillController from './WillsController';
+export * as MessageController from './MessageController';
